437e54bb8863d83314ca82fbedb9dd7143648ebb
Oleg Lymarchuk <oleg.lymarchuk@gmail.com>
2017-09-19 20:12:35 +0300

    QAFest

--- test_demo.py
+++ test_demo.py
@@ -1,5 +1,8 @@
 import pytest, time
 from helper.webuihelper import WebUITest
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 from selenium.webdriver.common.keys import Keys
 
 
@@ -8,6 +11,9 @@ class TestDemonstration(WebUITest):
     @pytest.mark.demo
     def test_demo(self, driver):
         self.driver.get('http://google.com')
-        self.driver.find_element_by_id('lst-ib').send_keys('Kyiv Docker Meetup')
+        self.driver.find_element_by_id('lst-ib').send_keys('QA Fest')
         self.driver.find_element_by_id('lst-ib').send_keys(Keys.ENTER)
-        time.sleep(5)
+        item = self.driver.find_element_by_xpath('//div[@class="g" and contains(., "qafest.com")]//h3/a')
+        item.click()
+        assert self.driver.find_elements_by_xpath('//*[contains(., "QA Fest 2017")]')
+        
