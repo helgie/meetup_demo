2b63f3e36474aa4f034275efd43cca98cc7afbf0
Oleg Lymarchuk <oleg.lymarchuk@gmail.com>
2017-10-19 20:23:54 +0300

    redwerk

--- test_demo.py
+++ test_demo.py
@@ -9,8 +9,8 @@ class TestDemonstration(WebUITest):
     @pytest.mark.demo
     def test_demo(self, driver):
         self.driver.get('http://google.com')
-        self.driver.find_element_by_id('lst-ib').send_keys('QA Fest')
+        self.driver.find_element_by_id('lst-ib').send_keys('Redwerk')
         self.driver.find_element_by_id('lst-ib').send_keys(Keys.ENTER)
-        item = self.driver.find_element_by_xpath('//div[@class="g" and contains(., "qafest.com")]//h3/a')
+        item = self.driver.find_element_by_xpath('//div[@class="g" and contains(., "redwerk.com")]//h3/a')
         item.click()
-        assert self.driver.find_elements_by_xpath('//*[contains(., "QA Fest 2017")]')
+        assert self.driver.find_elements_by_xpath('//*[contains(., "Custom Software Development Company")]')
