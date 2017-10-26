b438bf7471b865ea14aba0c7227273773032829a
Oleg Lymarchuk <oleg.lymarchuk@gmail.com>
2017-10-26 18:21:12 +0300

    Docker meetup

--- test_demo.py
+++ test_demo.py
@@ -9,8 +9,8 @@ class TestDemonstration(WebUITest):
     @pytest.mark.demo
     def test_demo(self, driver):
         self.driver.get('http://google.com')
-        self.driver.find_element_by_id('lst-ib').send_keys('Redwerk')
+        self.driver.find_element_by_id('lst-ib').send_keys('Kyiv Docker meetup  ')
         self.driver.find_element_by_id('lst-ib').send_keys(Keys.ENTER)
-        item = self.driver.find_element_by_xpath('//div[@class="g" and contains(., "redwerk.com")]//h3/a')
+        item = self.driver.find_element_by_xpath('//div[@class="g" and contains(., "Docker-Kyiv")]//h3/a')
         item.click()
-        assert self.driver.find_elements_by_xpath('//*[contains(., "Custom Software Development Company")]')
+        assert self.driver.find_elements_by_xpath('//*[contains(., "Kyiv Docker Meetup #2")]')
